39f204951d0cda48bb9a9bdadd2ed11d43899216
gcli <star106121@gmail.com>
2020-09-12 16:00:04 +0800

    Add 'func3()'.

--- main.cpp
+++ main.cpp
@@ -14,12 +14,20 @@ int func2(const int m, const int n) {
   return ret;
 }
 
+int func3(const double a, const double b) {
+  double ret = a - b;
+  ret += a * b;
+  ret += a + b;
+  return (int)ret;
+}
+
 int main() {
   int x = 1;
   int y = 2;
 
   printf("%d\n", func1(x, y));
   printf("%d\n", func2(x, y));
+  printf("%d\n", func3(x, y));
 
   return 0;
 }
