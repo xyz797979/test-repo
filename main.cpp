c8e9d2b4e3202e5da1f160230e3272c369349a79
gcli <star106121@gmail.com>
2020-09-12 15:51:53 +0800

    Add 'func2()'.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,19 @@ int func1(const int a, const int b) {
   return ret;
 }
 
+int func2(const int m, const int n) {
+  int ret = m + n;
+  ret += m * n;
+  ret += m - n;
+  return ret;
+}
+
 int main() {
   int x = 1;
   int y = 2;
 
   printf("%d\n", func1(x, y));
+  printf("%d\n", func2(x, y));
 
   return 0;
 }
