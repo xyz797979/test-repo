0a5f136a762bdb6f1c0dc33ed8108e8873e6ba19
gcli <star106121@gmail.com>
2020-09-12 17:09:55 +0800

    Add 'Func.h'.

---
+++ Func.h
@@ -0,0 +1,10 @@
+int func5() {
+  int ret = 10;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
+
--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include "Func.h"
+
 #include <stdio.h>
 
 int func1(const int a, const int b) {
@@ -26,12 +28,35 @@ int func2(const int m, const int n) {
   return ret;
 }
 
+int func3() {
+  int ret = 10;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
+
+int func4() {
+  int ret = 10;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
+
 int main() {
   int x = 1;
   int y = 2;
 
   printf("%d\n", func1(x, y));
   printf("%d\n", func2(x, y));
+  printf("%d\n", func3());
+  printf("%d\n", func4());
+  printf("%d\n", func5());
 
   return 0;
 }
