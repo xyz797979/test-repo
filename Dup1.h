04ee54bad4cab6a780b42f61dee4339255fa9ae6
gcli <star106121@gmail.com>
2020-09-14 10:00:48 +0800

    Yi-Yang Chao.

---
+++ Dup1.h
@@ -0,0 +1,19 @@
+int func6() {
+  int ret = 10;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
+
+int func8() {
+  int ret = 10;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
