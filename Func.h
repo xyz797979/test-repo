5488dab4d7885dba70b495c203ed1c8d0328a92d
gcli <star106121@gmail.com>
2020-09-15 17:11:40 +0800

    Add Func.h

---
+++ Func.h
@@ -0,0 +1,12 @@
+int func1(const int a, const int b) {
+  int ret = 0;
+  ret += a * b;
+  ret += a - b;
+  ret += a + b;
+  ret <<= 2;
+  ret += ret - 1;
+  ret -= 1;
+  ret *= ret;
+  ret >>= 1;
+  return ret;
+}
