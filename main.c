ad87d356da84e8d20b44e26aae2574dab59024cf
gcli <star106121@gmail.com>
2020-09-15 17:27:03 +0800

    Add fun2().

--- main.c
+++ main.c
@@ -8,6 +8,14 @@ int func1(const int a, const int b) {
   return ret;
 }
 
+int func2(const int a, const int b) {
+  int ret = 0;
+  ret += a * b;
+  ret += a - b;
+  ret += a + b;
+  return ret;
+}
+
 int main() {
   int x = 1;
   int y = 2;
