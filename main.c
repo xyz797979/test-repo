015fc162a12de94aeaca90e9bd6225691f3dba38
gcli <star106121@gmail.com>
2020-09-15 17:20:43 +0800

    Add func4() in main.c

--- main.c
+++ main.c
@@ -34,6 +34,14 @@ int func3(const int a, const int b) {
   return ret;
 }
 
+int func4(const int a, const int b) {
+  int ret = 0;
+  ret += a * b;
+  ret += a - b;
+  ret += a + b;
+  return ret;
+}
+
 int main() {
   int x = 1;
   int y = 2;
